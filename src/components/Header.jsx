4fa053cf8d42cf96fcf36b93702c9c5c4d097868
mtsdlf <mts@gmail.com>
2024-02-12 05:10:41 -0300

    development branch created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Filters } from './Filters.jsx'
 export function Header () {
   return (
     <header>
-      <h1>React Shop 🛒</h1>
+      <h1>Jugueteria Online 🛒</h1>
       <Filters />
     </header>
   )
